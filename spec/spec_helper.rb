03b08851dffe489d37d55b0da8b9f9637aee0c2a
Yet Another Clever Name <admin@yacn.pw>
2013-08-08 14:47:41 -0400

    added minitest/pride for colorful tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,11 @@ SimpleCov.start do
 end
 
 require 'minitest/autorun'
+begin
+  require 'minitest/pride' # colorful output
+rescue LoadError
+  # continue w/o color
+end
 if __FILE__ == $0
   $LOAD_PATH.unshift('lib', 'spec')
   Dir.glob('./spec/*_spec.rb') {|f| require f}
